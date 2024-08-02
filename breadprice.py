f37b96dffce302c6931494ad9bac9667b368d9af
Kyle Balbuena <kavbalbuena@mymail.mapua.edu.ph>
2024-08-02 17:21:25 +0800

    Update breadprice.py

--- breadprice.py
+++ breadprice.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the data from the CSV file
-file_path = 'breadprice.csv'  # Make sure this file is in the same directory as this script
+#data from CSV file
+file_path = 'breadprice.csv'  
 data = pd.read_csv(file_path)
 
-# Calculate the average price for each year
+#Average price for each year
 data['AveragePrice'] = data.mean(axis=1)
 
-# Extract the 'Year' and 'AveragePrice' columns
+#'Year' and 'AveragePrice' columns
 data = data[['Year', 'AveragePrice']]
 
-# Sort the data by year just in case
+# Sorting of data
 data = data.sort_values(by='Year')
 
-# Plot the data
+# Plotting of data
 plt.figure(figsize=(10, 5))
 plt.plot(data['Year'], data['AveragePrice'], marker='o')
 plt.title('Average Price of Bread Over Years')
